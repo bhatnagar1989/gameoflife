8278e8c72a5247ec9e769ecfb5aa937b67a11927
20176811 <20176811@nbwin1586.campus.tue.nl>
2017-11-22 16:15:23 +0100

    Added basic functions and attributes inside Cell Class

--- src/gameoflife/Cell.java
+++ src/gameoflife/Cell.java
@@ -2,9 +2,32 @@ package gameoflife;
 
 public class Cell {
 
+	public int x;
+	public int y;
+	public boolean isAlive;
+	public boolean newState;
+	public int numberOfAliveNeighbors;
+
+	
+	public Cell(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.isAlive = false;
+		this.newState = false;
+		this.numberOfAliveNeighbors = 0;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 0;
+	}
+
+	public int getX() {return this.x;}
+	public int getY() {return this.y;}
+	public boolean getCurrentState() {return this.isAlive;}
+	public void kill() {this.newState = false;}
+	public void reproduce() {this.newState = true;}
+	public void countAliveNeighbors() {
+		// TODO count the alive neighbors and place it inside this.numberOfAliveNeighbors
 	}
 
 }
